eb6d66f52b37a95a48769e85deef8c59138c95cf
Big Head <spencer.moyer16@yahoo.com>
2021-02-10 12:06:03 -0800

    added getPlayerName function

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// set player name to a proper value
+var getPlayerName = function() {
+    var name = "";
+    
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -60,6 +72,7 @@ var startGame = function() {
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round" + ( i + 1) );
+            debugger;
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
             fight(pickedEnemyObj);
